aaad930cece65fd5aa090494dd3d81c2afebc8cb
crazymaster <ken.i54k@gmail.com>
2012-11-08 17:57:53 +0900

    Updated main

--- main.cpp
+++ main.cpp
@@ -3,20 +3,26 @@
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QPushButton>
+#include <QLineEdit>
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
     QWidget* window = new QWidget;
     QLabel* label = new QLabel("<h1>Hello Qt!</h1>");
-    QPushButton* buttonA = new QPushButton("buttonA");
-    QPushButton* buttonB = new QPushButton("buttonB");
+    QPushButton* buttonA = new QPushButton("Quit");
+    QLineEdit* edit = new QLineEdit;
 
+    QObject::connect(buttonA, SIGNAL(clicked()), &app, SLOT(quit()) );
+    QObject::connect(edit, SIGNAL(textChanged(QString)), label, SLOT(setText(QString)) );
+    QObject::connect(edit, SIGNAL(textChanged(QString)), window, SLOT(setWindowTitle(QString)) );
+
+            
     QVBoxLayout* mainLayout = new QVBoxLayout;
     QHBoxLayout* layout = new QHBoxLayout;
 
+    layout->addWidget(edit);
     layout->addWidget(buttonA);
-    layout->addWidget(buttonB);
 
     mainLayout->addWidget(label);
     mainLayout->addLayout(layout);
